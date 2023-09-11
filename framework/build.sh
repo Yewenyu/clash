eee0681c7d6d31aef1761713aa2787c6b29bd231
ADDICE <addice.ch@gmail.com>
2023-09-11 15:30:47 +0800

    指定 iOS 最低支持版本 iOS 11, gomobile 暂不支持 macOS 指定最低版本

--- framework/build.sh
+++ framework/build.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 
+# make PP=g++ \
+#     CC=gcc \
+#     CFLAGS="-mmacosx-version-min=10.14 -mios-version-min=11.0"
+    # LFLAGS="-arch x86_64 -mmacosx-version-min=10.14 -mios-version-min=11.0 -Wl,-Bsymbolic-functions" static
 
 go get golang.org/x/mobile
 gomobile init
-gomobile bind  -target=ios,macos,iossimulator -o=framework/Clash.xcframework -bootclasspath=.. -v
+gomobile bind  -target=ios,macos,iossimulator -o=framework/Clash.xcframework -bootclasspath=.. -iosversion=11.0 -v
 
 # python3 build_clash_universal.py
