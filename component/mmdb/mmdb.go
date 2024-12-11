eac2287d68212d3efd2ea0103c8fc4b8c8692f29
yewenyu <289193866@qq.com>
2024-12-11 12:21:58 +0800

    mmdb

--- component/mmdb/mmdb.go
+++ component/mmdb/mmdb.go
@@ -1,6 +1,10 @@
 package mmdb
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"sync"
 
 	C "github.com/Dreamacro/clash/constant"
@@ -34,6 +38,7 @@ func Verify() bool {
 
 func Instance() *geoip2.Reader {
 	once.Do(func() {
+		InitMMDB()
 		var err error
 		mmdb, err = geoip2.Open(C.Path.MMDB())
 		if err != nil {
@@ -42,4 +47,43 @@ func Instance() *geoip2.Reader {
 	})
 
 	return mmdb
+}
+
+func downloadMMDB(path string) (err error) {
+	resp, err := http.Get("https://cdn.jsdelivr.net/gh/Dreamacro/maxmind-geoip@release/Country.mmdb")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, resp.Body)
+
+	return err
+}
+
+func InitMMDB() error {
+	if _, err := os.Stat(C.Path.MMDB()); os.IsNotExist(err) {
+		log.Infoln("Can't find MMDB, start download")
+		if err := downloadMMDB(C.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't download MMDB: %s", err.Error())
+		}
+	}
+
+	if !Verify() {
+		log.Warnln("MMDB invalid, remove and download")
+		if err := os.Remove(C.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't remove invalid MMDB: %s", err.Error())
+		}
+
+		if err := downloadMMDB(C.Path.MMDB()); err != nil {
+			return fmt.Errorf("can't download MMDB: %s", err.Error())
+		}
+	}
+
+	return nil
 }
