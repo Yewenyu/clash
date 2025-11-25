f7586711f31d4c8cb66b3747f2085de0be368830
yewenyu <289193866@qq.com>
2025-11-25 12:16:59 +0800

    fix crash

--- common/net/relay.go
+++ common/net/relay.go
@@ -1,7 +1,6 @@
 package net
 
 import (
-	"io"
 	"net"
 	"runtime"
 	"sort"
@@ -24,21 +23,6 @@ var (
 )
 
 // Relay copies between left and right bidirectionally.
-func Relay1(leftConn, rightConn net.Conn) {
-	ch := make(chan error)
-
-	go func() {
-		// Wrapping to avoid using *net.TCPConn.(ReadFrom)
-		// See also https://github.com/Dreamacro/clash/pull/1209
-		_, err := io.Copy(WriteOnlyWriter{Writer: leftConn}, ReadOnlyReader{Reader: rightConn})
-		leftConn.SetReadDeadline(time.Now())
-		ch <- err
-	}()
-
-	io.Copy(WriteOnlyWriter{Writer: rightConn}, ReadOnlyReader{Reader: leftConn})
-	rightConn.SetReadDeadline(time.Now())
-	<-ch
-}
 
 var TCPBufferSize = 0
 
@@ -64,54 +48,6 @@ func Relay(leftConn, rightConn net.Conn, useHttpTimeout bool, useDNSTimeout bool
 		activeTime: time.Now(),
 	}
 	queuePool.AddConns(info)
-}
-func Relay2(leftConn, rightConn net.Conn, useHttpTimeout bool, useDNSTimeout bool) {
-	if TCPBufferSize == 0 {
-		Relay1(leftConn, rightConn)
-		return
-	}
-	// handle(cChan, &handleO)
-	// 如果两个连接在指定时间内有数据交互，则继续连接，否则关闭连接
-	timeout := TcpTimeout
-	if useHttpTimeout {
-		timeout = HttpTimeout
-	}
-	if useDNSTimeout {
-		timeout = DNSTimeout
-	}
-	timeLock := time.NewTimer(time.Duration(timeout) * time.Second)
-
-	handle := func(w, r net.Conn) {
-		b := make([]byte, TCPBufferSize)
-		defer w.Close()
-	loop:
-		for {
-			select {
-			case <-timeLock.C:
-				break loop
-			default:
-				// 更新连接时间
-				timeLock.Reset(time.Duration(timeout) * time.Second)
-				r.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
-				n, err := r.Read(b)
-				if err != nil {
-					// 如果读取超时，继续循环
-					if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-						continue
-					}
-					break loop
-				}
-				_, err = w.Write(b[:n])
-				if err != nil {
-					break loop
-				}
-			}
-
-		}
-	}
-	go handle(rightConn, leftConn)
-	handle(leftConn, rightConn)
-	rightConn.SetReadDeadline(time.Now())
 }
 
 type ConnsInterface interface {
@@ -161,10 +97,16 @@ func (c *ConnsInfo) IsStop() bool {
 	return c.LeftConn == nil || c.RightConn == nil
 }
 func (c *ConnsInfo) Close() {
-	c.LeftConn.Close()
-	c.RightConn.Close()
-	c.LeftConn = nil
-	c.RightConn = nil
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.LeftConn != nil {
+		c.LeftConn.Close()
+		c.LeftConn = nil
+	}
+	if c.RightConn != nil {
+		c.RightConn.Close()
+		c.RightConn = nil
+	}
 }
 func (c *ConnsInfo) SetActiveTime(time time.Time) {
 	c.activeTime = time
@@ -176,23 +118,19 @@ func (connInfo *ConnsInfo) Relay() {
 
 	defer func() {
 		rightConn.SetReadDeadline(time.Now())
-		connInfo.lock.Lock()
 		connInfo.Close()
-		connInfo.lock.Unlock()
 		runtime.GC()
 	}()
 	buf := make([]byte, 10)
 	n, err := leftConn.Read(buf)
 	if err != nil {
-		rightConn.Close()
-		leftConn.Close()
+		connInfo.Close()
 		return
 	}
 	rightConn.Write(buf[:n])
 	handle := func(w, r net.Conn) {
 		b := pool.Get(TCPBufferSize)
 		defer pool.Put(b)
-		defer w.Close()
 	loop:
 		for {
 			connInfo.lock.Lock()
@@ -373,3 +311,67 @@ func (p *RunPool) relay(connInfo ConnsInterface) {
 	connInfo.Relay()
 
 }
+
+// func Relay1(leftConn, rightConn net.Conn) {
+// 	ch := make(chan error)
+
+// 	go func() {
+// 		// Wrapping to avoid using *net.TCPConn.(ReadFrom)
+// 		// See also https://github.com/Dreamacro/clash/pull/1209
+// 		_, err := io.Copy(WriteOnlyWriter{Writer: leftConn}, ReadOnlyReader{Reader: rightConn})
+// 		leftConn.SetReadDeadline(time.Now())
+// 		ch <- err
+// 	}()
+
+// 	io.Copy(WriteOnlyWriter{Writer: rightConn}, ReadOnlyReader{Reader: leftConn})
+// 	rightConn.SetReadDeadline(time.Now())
+// 	<-ch
+// }
+// func Relay2(leftConn, rightConn net.Conn, useHttpTimeout bool, useDNSTimeout bool) {
+// 	if TCPBufferSize == 0 {
+// 		Relay1(leftConn, rightConn)
+// 		return
+// 	}
+// 	// handle(cChan, &handleO)
+// 	// 如果两个连接在指定时间内有数据交互，则继续连接，否则关闭连接
+// 	timeout := TcpTimeout
+// 	if useHttpTimeout {
+// 		timeout = HttpTimeout
+// 	}
+// 	if useDNSTimeout {
+// 		timeout = DNSTimeout
+// 	}
+// 	timeLock := time.NewTimer(time.Duration(timeout) * time.Second)
+
+// 	handle := func(w, r net.Conn) {
+// 		b := make([]byte, TCPBufferSize)
+// 		defer w.Close()
+// 	loop:
+// 		for {
+// 			select {
+// 			case <-timeLock.C:
+// 				break loop
+// 			default:
+// 				// 更新连接时间
+// 				timeLock.Reset(time.Duration(timeout) * time.Second)
+// 				r.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
+// 				n, err := r.Read(b)
+// 				if err != nil {
+// 					// 如果读取超时，继续循环
+// 					if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+// 						continue
+// 					}
+// 					break loop
+// 				}
+// 				_, err = w.Write(b[:n])
+// 				if err != nil {
+// 					break loop
+// 				}
+// 			}
+
+// 		}
+// 	}
+// 	go handle(rightConn, leftConn)
+// 	handle(leftConn, rightConn)
+// 	rightConn.SetReadDeadline(time.Now())
+// }
